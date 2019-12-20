a9410d1d3347dea39e1130141152094e39ca9782
jungwoo <jungwoo5759@gmail.com>
2019-12-20 22:49:56 +0900

    code plus dp solve update

--- baekjoon/code_plus/basic1/dp/16194.cc
+++ baekjoon/code_plus/basic1/dp/16194.cc
@@ -1,11 +1,26 @@
 /*
 [BOJ] 16194. 카드 구매하기2
+memset 쓰면 틀림;; 왜지...
 */
 #include <cstdio>
+#include <cstring>
+#include <algorithm>
 using namespace std;
+
+int n, cardP[1001], dp[1001];
 
 int main()
 {
-    
+    scanf("%d", &n);
+    fill(dp, dp+n+1, 1e9);
+    for(int i=1;i<=n;i++) scanf("%d", &cardP[i]);
+    dp[0] = 0;
+    dp[1] = cardP[1];
+    for(int i=2;i<=n;i++) {
+        for(int j=1;j<=i;j++){
+            dp[i] = min(dp[i], dp[i-j] + cardP[j]);
+        }
+    }
+    printf("%d\n", dp[n]);
     return 0;
 }
--- baekjoon/code_plus/basic2/15663.cc
+++ baekjoon/code_plus/basic2/15663.cc
@@ -1,5 +1,5 @@
 /*
-[BOJ] 15657. N과 M(9)
+[BOJ] 15663. N과 M(9)
 */
 #include <cstdio>
 #include <vector>
@@ -9,28 +9,27 @@ const int MAX =  8;
 int n,m;
 bool visited[MAX+1];
 int outputA[MAX+1];
-vector<int> arr;
 int hash_arr[MAX+1];
+vector<int> arr;
 
 void dfs(int cnt){
 
     if(cnt == m){
         for(int i=0;i<m;i++){
-//            if(!visited[arr[i]]) {
-                printf("%d ", outputA[i]);
-//            }
+            printf("%d ", outputA[i]);
         }
         printf("\n");
         return;
     }
 
     for(int i=0;i<n;i++){
-        if(hash_arr[arr[i]]) {
-            hash_arr[arr[i]]++;
+        if(hash_arr[arr[i]]){ //&& !visited[i]) {
+            hash_arr[arr[i]]--;
             visited[i] = true;
             outputA[cnt] = arr[i];
             dfs(cnt + 1);
-            hash_arr[arr[i]]--;
+            visited[i] = false;
+            hash_arr[arr[i]]++;
         }
     }
 }
