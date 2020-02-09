6b9ce54994eb47158bd16dbfa497c10065dfdd8c
jungwoo <jungwoo5759@gmail.com>
2020-02-09 22:52:51 +0900

    boj simul solve update

--- baekjoon/simul/17281.cpp
+++ baekjoon/simul/17281.cpp
@@ -10,12 +10,86 @@
 using namespace std;
 #define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-int n, start;
-int map[9];
+int n, locate, ans;
+int player[50][9], seq[9];
+bool visited[9];
 
-void bfs(int idx){
-    for(int i=0;i<idx;i++){
-        cout<<map[i]<<"\n";
+int bfs(){
+    int out = 0, inning = 0, score = 0;
+    int ground[4] = {0,};
+    locate = 0;
+    while(1){
+        
+        //cout<<locate<<" inning:"<<inning<<" out:"<<out<<", "<<player[inning][seq[locate]]<<"\n";
+        switch(player[inning][seq[locate]]){
+            case 0: // out
+            out++;
+            break;
+            case 1:
+            ground[3] = ground[2];
+            ground[2] = ground[1];
+            ground[1] = ground[0];
+            ground[0] = 1;
+            break;
+            case 2:
+            ground[3] = ground[2] + ground[1];
+            ground[2] = ground[0];
+            ground[1] = 1;
+            ground[0] = 0;
+            break;
+            case 3:
+            ground[3] = ground[2] + ground[1] + ground[0];
+            ground[2] = 1;
+            ground[1] = 0;
+            ground[0] = 0;
+            break;
+            case 4:
+            ground[3] = ground[2] + ground[1] + ground[0] + 1;
+            ground[2] = 0;
+            ground[1] = 0;
+            ground[0] = 0;
+            break;
+        }
+
+        score += ground[3];
+        ground[3] = 0;
+
+        if(out == 3){
+            //cout<<score<<", "<<inning<<"\n";
+            inning++;
+            out = 0;
+            memset(ground, 0, sizeof(ground));
+        }
+
+        if(inning == n){
+            return score;
+        }
+        //locate = (locate + 1) % 9;
+        locate++;
+        if(locate == 9){
+            locate = 0;
+        }
+    }
+}
+
+void dfs(int cnt){
+    if(cnt == 9){
+        ans = max(ans, bfs());
+    }
+
+    if(cnt == 3) {  
+        seq[cnt] = 0;
+        dfs(cnt + 1);
+    }
+    else {
+        for(int i=1;i<9;i++){
+            if(!visited[i]){
+                visited[i] = true;
+                seq[cnt] = i;
+                dfs(cnt+1);
+                visited[i] = false;
+            }
+        }
     }
 }
 
@@ -24,12 +98,13 @@ int main()
     io;
     cin>>n;
     for(int i=0;i<n;i++){
-        int map[9];
         for(int j=0;j<9;j++){
-            cin>>map[j];
-            bfs(j);
+            cin>>player[i][j];
         }
     }
+
+    dfs(0);
+    cout<<ans<<"\n";
 
     return 0;
 }
--- baekjoon/simul/1938.cpp
+++ baekjoon/simul/1938.cpp
@@ -1,13 +1,14 @@
 /*
-[BOJ] 14889. 스타트와 링크
+[BOJ] 1938. 통나무 옮기기
 */
 
 #include <iostream>
 #include <algorithm>
+#include <queue>
 using namespace std;
 #define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-int n;
+int n, ans;
 char map[51][51];
 
 struct wood{
@@ -16,11 +17,58 @@ struct wood{
 };
 
 wood my;
-
+wood des;
 int dir[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
 void bfs(){
+    queue<pair<pair<int, int>, pair<int, int> > > qp;
+    qp.push(make_pair(make_pair(my.x, my.y), make_pair(my.dir, 0));
+    while(!qp.empty()){
+        int x = qp.front().first.first, y = qp.front().first.second;
+        int dir = qp.front().second.first, cnt = qp.front().second.second;
+        if(des.x == x && des.y == y && des.dir == dir){
+            ans = min(ans, cnt);
+        }
+        qp.pop();
+        for(int i=0;i<5;i++){
+            if(i == 4){ // turn
+                if(!turn_check(mx,my,dir)) continue;
+                if(dir) qp.push(make_pair(make_pair(mx,my),make_pair(0, cnt+1)));
+                else qp.push(make_pair(make_pair(mx,my),make_pair(1, cnt+1))); 
+            }else {
+                int mx = x + dir[i][0], my = y + dir[i][1];
+                if(mx < 0 || my < 0 || mx >= n || my >= n) continue;
+                if(!move_check(mx,my,dir)) continue;
+                qp.push(make_pair(make_pair(mx,my),make_pair(dir, cnt+1)));
+            }
+        }
+    }
+}
 
+bool turn_check(int x, int y, int dir){   // dir 0 :가로, 1: 세로
+    if(dir){
+        if(x-1 < 0 || x+1 >= n || map[x-1][y] == 1 || map[x+1][y] == 1){
+            return false;
+        }
+    }else {
+        if(y-1 < 0 || y+1 >= n || map[x][y-1] == 1 || map[x][y+1] == 1){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool move_check(int x, int y, int dir){   // dir 0 :가로, 1: 세로
+    if(dir){
+        if(y-1 < 0 || y+1 >= n || map[x][y-1] == 1 || map[x][y+1] == 1){
+            return false;
+        }
+    }else{
+        if(x-1 < 0 || x+1 >= n || map[x-1][y] == 1 || map[x+1][y] == 1){
+            return false;
+        }
+    }
+    return true;
 }
 
 int main()
@@ -30,24 +78,43 @@ int main()
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             cin>>map[i][j];
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
             if(map[i][j] == 'B'){
-                if(i+1 < n || i-1 > 0 || j+1<n || j-1 > 0){
-                    if(map[i+1][j] == 'B' || map[i-1][j] == 'B'){
-                        my.x = i;
-                        my.y = j;
-                        my.dir = 1;
-                    }
-                    else if(map[i][j+1] == 'B' || map[i][j-1] == 'B'){
-                        my.x = i;
-                        my.y = j;
-                        my.dir = -1;
-                    }
+                if(i+1 < n && i-1 >= 0 &&
+                map[i+1][j] == 'B' && map[i-1][j] == 'B'){
+                    my.x = i;
+                    my.y = j;
+                    my.dir = 1;
+                }
+                else if(j+1<n && j-1 >= 0 && 
+                map[i][j+1] == 'B' && map[i][j-1] == 'B'){
+                    my.x = i;
+                    my.y = j;
+                    my.dir = 0;
+                }
+            }
+            else if(map[i][j] == 'E'){
+                if(i+1 < n && i-1 >= 0 &&
+                map[i+1][j] == 'E' && map[i-1][j] == 'E'){
+                    des.x = i;
+                    des.y = j;
+                    des.dir = 1;
+                }
+                else if(j+1<n && j-1 >= 0 && 
+                map[i][j+1] == 'E' && map[i][j-1] == 'E'){
+                    des.x = i;
+                    des.y = j;
+                    des.dir = 0;
                 }
             }
         }
     }
-
+    
     bfs();    
-
+    cout<<ans<<"\n";
     return 0;
 }
