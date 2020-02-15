53c36942b91355386623471d683658e44796621f
hywook4 <hywook4@gmail.com>
2020-02-15 22:29:48 +0900

    code plus intermediate code update

---
+++ baekjoon/code_plus/intermediate1/brute-force/14888.cpp
@@ -0,0 +1,23 @@
+/*
+[BOJ] 14888. 연산자 끼워넣기
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int n;
+int numbers[11];
+
+int main()
+{   
+    io;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>numbers[i];
+    }
+
+    return 0;
+}
