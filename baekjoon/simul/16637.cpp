5bafe22cc3864b9701edfecd273453498ac25f69
jungwoo <jungwoo5759@gmail.com>
2020-02-18 13:03:11 +0900

    boj simul solve update

--- baekjoon/simul/16637.cpp
+++ baekjoon/simul/16637.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <algorithm>
+#include <string>
 #include <cmath>
 using namespace std;
 #define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
@@ -11,19 +12,18 @@ using namespace std;
 char formula[19];
 int number[19], result[19];
 int n, fcnt, ncnt, ans;
-bool visited[19];
 
 int calc(int a, int b, char form){
     int res = 0;
 
     switch(form){
-        case '+';
+		case '+':
         res = a+b;
         break;
-        case '-';
+		case '-':
         res = a-b;
         break;
-        case '*';
+		case '*':
         res = a*b;
         break;
     }
@@ -31,29 +31,46 @@ int calc(int a, int b, char form){
     return res;
 }
 
-void solve(int cnt, int sum){
-    if(cnt == fcnt){
-        ans = max(ans, sum);
-    }
+void dfs(int cnt, int sum){
 
-    for(int i=0;i<ncnt;i++){
-        if(!visited[i]){
-            visited[i] = true;
-            
-            visited[i] = false;
-        }
+    if(cnt >= fcnt){
+        ans = max(ans, sum);
+		return;
     }
+	
+	int res = calc(sum, number[cnt+1], formula[cnt]);
+	dfs(cnt+1, res);
+
+	if(cnt+1 < fcnt){
+		int af_res = calc(number[cnt+1], number[cnt+2], formula[cnt+1]);
+		int cur_res = calc(sum, af_res, formula[cnt]);
+		dfs(cnt+2, cur_res);
+	}
+}
+
+void solve(){
+	if(n == 1){
+		cout<<number[0]<<"\n";
+	}
+	else if(n == 3){
+		cout<<calc(number[0], number[1], formula[0])<<"\n";
+	}
+	dfs(0, number[0]);
+	cout<<ans<<"\n";
 }
 
 int main()
 {
     io;
     cin>>n;
+	
+	string input_formula;
+	cin>>input_formula;
     for(int i=0;i<n;i++){
-        if(i%2==0) cin>>formula[fcnt++];
-        else cin>>number[ncnt++];
+        if(i%2==0) number[ncnt++] = input_formula[i] - '0';
+        else formula[fcnt++] = input_formula[i];
     }
-    solve(0);
-    cout<<ans<<"\n";
+
+    solve();
     return 0;
 }
