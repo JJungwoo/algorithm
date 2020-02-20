2bc15a037c6f6eabfd3e7c3fbb759f938ed7b296
jungwoo <jungwoo5759@gmail.com>
2020-02-20 12:59:16 +0900

    boj code plus intermdiate bruto-force solve update

--- baekjoon/code_plus/intermediate1/brute-force/9663.cpp
+++ baekjoon/code_plus/intermediate1/brute-force/9663.cpp
@@ -4,16 +4,46 @@
 
 #include <iostream>
 #include <algorithm>
-#include <cmath>
+#include <cstring>
 using namespace std;
 #define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-int n;
-int map[16][16];
-int dir
-
-void dfs(int cnt)
+int n, ans;
+bool visited[16][16];
+int dir[8][2] = 
 {
+	{-1,-1}, {-1,0}, {-1,1},
+	{0, -1}, {0, 1},
+	{1, -1}, {1, 0}, {1 ,1}
+};
+
+bool move_check(int x, int y)
+{
+	for(int i=0;i<8;i++){
+		int mx = x + dir[i][0], my = y + dir[i][0];
+		if(mx < 0 || my < 0 || mx >= n || my >= m) return false;
+		if(visited[mx][my]) return false;
+
+	}
+	return true;
+}
+
+void dfs(int x, int y, int cnt)
+{
+	if(cnt == n){
+		ans++;
+	}
+
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(visited[i][j]) continue;
+			if(move_check == false) continue;			
+			visited[i][j] = true;
+			dfs(i, j, cnt+1);
+			visited[i][j] = false;
+		}
+	}
+		
 
 }
 
@@ -21,7 +51,13 @@ int main()
 {
         io;
         cin>>n;
-        dfs(0);
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+        		dfs(i, j, 1);
+				memset(visited, 0, sizeof(visited));
+			}
+		}
+		cout<<ans<<"\n";
 
         return 0;
 }
