8eb08f0a1536cd4740b256839d24b444c34f8756
JJungwoo <jungwoo5759@gmail.com>
2020-03-07 22:05:55 +0900

    boj code plus im step solve update

--- baekjoon/simul/17135.cpp
+++ baekjoon/simul/17135.cpp
@@ -4,6 +4,7 @@
 같은 적이 여러 궁수에게 공격당할수 있고 궁수가 공격하는 적은 거리가 D이하인 적중에
 가장 가까운 적이고, 그러한 적이 여럿일 경우 가장 왼쪽에 적을 공격한다.
 궁수의 공격이 끝나고 적이 이동한다.
+disk = |n - i| + |k - j|
 */
 #include <iostream>
 #include <vector>
@@ -16,6 +17,15 @@ int n, m, d, ans;
 int map[16][16];
 bool visited[16];
 vector<int> archer;
+
+struct castle{
+    int x, y, z;
+    bool operator < (const castle &t) const {
+        if(z == t.z) return y > t.y;
+        else return z > t.z;
+    }
+};
+
 void move_monster() {
 	for (int i = n-1; i >= 0; i--) {
 		for (int j = m - 1; j >= 0; j--) {
@@ -28,6 +38,7 @@ void move_monster() {
 		}
 	}
 }
+
 void print_map() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
@@ -36,13 +47,17 @@ void print_map() {
 		cout << "\n";
 	}
 }
+
 void hunting_monster(int pos) {
+
 }
+
 void dfs(int cnt) {
 	if (cnt == 3) {
 		for (int i = 0; i < n; i++) {
 			vector<pair<int, int> > killed;
 			for (int j = 0; j < archer.size(); j++) {
+
 			}
 		}
 		ans = max(ans, cnt);
@@ -57,6 +72,7 @@ void dfs(int cnt) {
 		visited[i] = false;
 	}
 }
+
 int main() 
 {
 	cin >> n >> m >> d;
---
+++ baekjoon/simul/17136.cpp
@@ -0,0 +1,146 @@
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <cmath>
+using namespace std;
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define cpMax 10
+
+int map[10][10];
+
+int main()
+{
+    io;
+    for(int i=0;i<cpMax;i++){
+        for(int j=0;j<cpMax;j++){
+            cin>>map[i][j];
+        }
+    }
+
+    return 0;
+}
+
+/*
+[boj] 17136. 색종이 붙이기
+전역 배열로 풀이 방법: https://hororolol.tistory.com/82
+함수에 배열 인자값 넘기는 방법: https://regularmember.tistory.com/102
+*/
+#include <iostream>
+#include <vector>
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int ans = 50, total_cnt;
+int map[10][10];
+void solve(int cnt) 
+{
+	if (cnt >= total_cnt) return;
+	pair<int, int> p;
+	p.first = 10;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (map[i][j] == 1) {
+				p = make_pair(i, j);
+				break;
+			}
+		}
+	}
+	if (p.first != 10) {
+	}
+	if (ans > cnt) {
+		ans = cnt;
+	}
+}
+int main()
+{
+	io;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			cin >> map[i][j];
+			if (map[i][j]) {
+				total_cnt++;
+			}
+		}
+	}
+	solve(0);
+	return 0;
+}
+/*
+int ans = 987654321;
+int map[11][11];
+int cpaper[5];
+vector<pair<int, int> > vp;
+vector<pair<int, int> > remove_vp;
+void recovery_paper() {
+	for (int i = 0; i < remove_vp.size(); i++) {
+		if (map[remove_vp[i].first][remove_vp[i].second] == 0)
+			map[remove_vp[i].first][remove_vp[i].second] == 1;
+	}
+}
+void copy_map(int tmp_map[10][10]) {
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			tmp_map[i][j] = map[i][j];
+		}
+	}
+}
+bool check_paper(int value, int x, int y)
+{
+	for()
+	return true;
+}
+bool map_check(int tmp_map[10][10])
+{
+	for (int i = 0; i < cpMax; i++) {
+		for (int j = 0; j < cpMax; j++) {
+			if (tmp_map[i][j]) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+bool solve(bool exit, int tmp_map[10][10])
+{
+	if (exit) {
+		int total = 0;
+		for (int i = 0; i < 5; i++)
+			total += (5 - cpaper[i]);
+		if (ans > total) ans = total;
+		return;
+	}
+	for (int z = 0; z < vp.size(); z++) {
+		if (map[vp[z].first][vp[z].second] == 0) continue;
+		for (int i = 0; i < 5; i++) {
+			if (cpaper[i]) {
+				if (check_paper(i, vp[z].first, vp[z].second)) {
+					cpaper[i]--;
+					int tmp_map[10][10];
+					copy_map(tmp_map);
+					solve(!map_check(tmp_map));
+					recovery_paper();
+					cpaper[i]++;
+				}
+			}
+		}
+	}
+}
+int main()
+{
+	io;
+	for (int i = 0; i < 5; i++)
+		cpaper[i] = 5;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			cin >> map[i][j];
+			if (map[i][j]) {
+				vp.push_back(make_pair(i, j));
+			}
+		}
+	}
+	solve(false);
+	cout << ans << "\n";
+	return 0;
+}
+*/
