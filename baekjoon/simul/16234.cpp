d77ef49951146b08972814bdd0267a2e8fe8bb44
JJungwoo <jungwoo5759@gmail.com>
2020-03-28 11:28:16 +0900

    - boj simul 16234. 인구 이동 solve update (무난한 시뮬 + bfs 문제, 구간 나눠서 계산하는 문제)

--- baekjoon/simul/16234.cpp
+++ baekjoon/simul/16234.cpp
@@ -1,95 +1,202 @@
 /*
-[BOJ] 16234. 인구 이동
-국경선을 공유하는 두 나라의 인구 차이가 L명 이상, R명 이하라면, 두 나라가 공유하는 국경선을 오늘 하루동안 연다.
-(연합의 인구수)/(연합을 이루고 있는 칸의 개수)
+[boj] 16234. �α� �̵�
 */
 
 #include <iostream>
-#include <algorithm>
 #include <cstring>
-#include <vector>
-using namespace std;
+#include <queue>
 #define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
 
-int n, l, r, ans, cnt, team_value; // L up, R down 
-int map[51][51];
-int team[51][51];
-int dir[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+int n, l, r, ans, cnt;
+int map[50][50];
+int dir[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
+bool visited[50][50];
+bool move_check;
 
-inline bool check_move(int x1, int y1, int x2, int y2){
-    int diff = abs(map[x1][y1] - map[x2][y2]);
-    if(diff >= l && diff <= r) return true;
-    else return false;
+inline bool gocheck(int val1, int val2) {
+	int diff = abs(val1 - val2);
+	if (diff >= l && diff <= r) return true;
+	return false;
 }
 
-void dfs(int x, int y){
-    if(team[x][y]) return;
-    team[x][y] = true;
-
-    for(int i=0;i<4;i++){
-        int mx = x + dir[i][0], my = y + dir[i][1];
-        if(mx < 0 || my < 0 || mx >= n || my >= n) continue;
-        if(team[mx][my]) continue;
-        if(!check_move(x,y,mx,my)) continue;
-        cnt++;
-        team[mx][my] = true;
-        team_value += map[mx][my];
-    }
+int cal_population(int x, int y, vector<pair<int, int> > &vp) {
+	int sum = map[x][y];
+	queue<pair<int, int> > qp;
+	qp.push(make_pair(x, y));
+	visited[x][y] = true;
+	while (!qp.empty()) {
+		int dx = qp.front().first, dy = qp.front().second;
+		qp.pop();
+		for (int i = 0; i < 4; i++) {
+			int mx = dx + dir[i][0], my = dy + dir[i][1];
+			if (mx < 0 || my < 0 || mx >= n || my >= n) continue;
+			if (visited[mx][my]) continue;
+			if (!gocheck(map[mx][my], map[dx][dy])) continue;
+			visited[mx][my] = true;
+			sum += map[mx][my];
+			qp.push(make_pair(mx, my));
+			vp.push_back(make_pair(mx, my));
+			cnt++;
+		}
+	}
+	if (sum == map[x][y]) return 0;
+	return sum;
 }
 
-void divide(int val){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(team[i][j]){
-                map[i][j] = val;
-                team[i][j] = false;
-            }
-        }
-    }
+void print_map() {
+	cout << "print_map\n";
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << map[i][j] << " ";
+		}
+		cout << "\n";
+	}
 }
 
-bool check_map(){
-    bool check = false;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(!team[i][j]){
-                cnt = 1;
-                dfs(i,j);
-                if(cnt > 1){
-                    team_value /= cnt;
-                    divide(team_value);
-                    check = true;
-                }
-            }
-        }
-    }
-    if(check) return true;
-    return false;
-}
-
-void solve(){
-    while(true){
-        bool moved = false;
-        memset(team, 0, sizeof(team));
-        team_value = 0;
-        if(check_map()){
-            ans++;
-        }else {
-            break;
-        }
-    }
+void solve() {
+	while (1) {
+		memset(visited, 0, sizeof(visited));
+		move_check = false;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (visited[i][j]) continue;
+				vector<pair<int, int> > vp;
+				vp.push_back(make_pair(i, j));
+				cnt = 1;
+				int total = cal_population(i, j, vp);
+				if (total != 0) {
+					int value = total/cnt;
+					int size = vp.size();
+					for (int v = 0; v < size; v++) {
+						map[vp[v].first][vp[v].second] = value;
+					}
+					move_check = true;
+				}
+			}
+		}
+		if(move_check) ans++;
+		else break;
+	}
 }
 
 int main()
 {
-    io;
-    cin>>n>>l>>r;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin>>map[i][j];
-        }
-    }
-    solve();
-    cout<<ans<<"\n";
-    return 0;
+	io;
+	cin >> n >> l >> r;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> map[i][j];
+		}
+	}
+	solve();
+	cout << ans << "\n";
+	return 0;
 }
+
+
+/*
+#include <iostream>
+#include <cstring>
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+int n, l, r, ans, cnt, total;
+int map[51][51];
+bool visited[51][51], team[51][51];
+int dir[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
+
+
+inline bool check_move(int x1, int y1, int x2, int y2) {
+	int diff = abs(map[x1][y1] - map[x2][y2]);
+	if (diff >= l && diff <= r) return true;
+	return false;
+}
+
+void dfs(int x, int y) {
+	for (int i = 0; i < 4; i++) {
+		int mx = x + dir[i][0], my = y + dir[i][1];
+		if (mx < 0 || my < 0 || mx >= n || my >= n) continue;
+		if (team[mx][my]) continue;
+		if (!check_move(x, y, mx, my)) continue;
+		team[mx][my] = true;
+		cnt++;
+		total += map[mx][my];
+		dfs(mx, my);
+	}
+}
+
+void divide(int val) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (team[i][j]) {
+				//cout << i << "," << j << "," << val << "\n";
+				map[i][j] = val;
+				team[i][j] = false;
+			}
+		}
+	}
+}
+
+void print_map() {
+	cout << "\n";
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << map[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+
+bool check_map() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			//total = 0;
+			if (!visited[i][j]) {
+				visited[i][j] = true;
+				total = map[i][j];
+				cnt = 1;
+				dfs(i, j);	// �湮 üũ
+				if (cnt > 1) {		// ���� ���� ���� ����
+					int div_val = total / cnt;
+					//cout << cnt << "," << total << "\n";
+					divide(div_val);
+					//check = true;
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+void solve() {
+	while (true) {
+		bool check = false;
+		memset(visited, 0, sizeof(visited));
+		if (check_map()) {
+			ans++;
+		}
+		else break;
+		print_map();
+		//cout << ans << "\n";
+		//if (check) ans++;
+		
+	}
+}
+
+int main()
+{
+	io;
+	cin >> n >> l >> r;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> map[i][j];
+		}
+	}
+	solve();
+	cout << ans << "\n";
+
+	return 0;
+}
+*/
