b16ed6b8fa52e496a21ae5abd42f05a82bc24a64
JJungwoo <jungwoo5759@gmail.com>
2020-03-31 22:53:54 +0900

    - boi simul 17135. 캐슬 디펜스 update (대각선에서 최소 값을 중복 포함해서 리스트 만들어야함.. 구현하는 방법이 익숙하지 않아서 엄청 헤멤.. 다시꼭 풀어야함.. 아직 해결안됨..)

--- baekjoon/simul/17135.cpp
+++ baekjoon/simul/17135.cpp
@@ -4,8 +4,108 @@
 같은 적이 여러 궁수에게 공격당할수 있고 궁수가 공격하는 적은 거리가 D이하인 적중에
 가장 가까운 적이고, 그러한 적이 여럿일 경우 가장 왼쪽에 적을 공격한다.
 궁수의 공격이 끝나고 적이 이동한다.
-disk = |n - i| + |k - j|
+d = |n - i| + |k - j|
+
+해결 방법
+1. dfs로 경로 찾아서 공격 
+2. 2중 포문으로 하나씩 다 거리 체크해서 공격
+3. 공격 대상들 리스트만들어서 정렬한다음 작은 값부터 공격
+
 */
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <queue>
+using namespace std;
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int n,m,d,ans;
+int map[16][16], tmp_map[16][16];
+vector<int> hunters;
+inline int cal_dis(int x1, int y1, int x2, int y2){
+	return abs(x1-x2) + abs(y1-y2);
+}
+void copy_map(){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			tmp_map[i][j] = map[i][j];
+		}
+	}
+}
+void print_map(){
+	cout<<"print_map\n";
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			cout<<tmp_map[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+}
+void solve(int cur, int cnt){
+	if(cnt == 3){
+		//cout<<"debug:"<<hunters[0]<<","<<hunters[1]<<","<<hunters[2]<<"\n";
+		int sum = 0;
+		copy_map();
+		for(int i=n;i>0;i--){
+			vector<pair<int, int> > kill;
+			for(int j=0;j<3;j++){
+				int tj = hunters[j], value = 16*16;
+				int dx = 0, dy = 16;
+				for(int z=1;z<=d;z++){
+					for(int y=0;y<m;y++){
+						if(i-z < 0) continue;
+						int tmp = cal_dis(i, tj, i-z, y);
+						if(tmp <= d && tmp_map[i-z][y] == 1){
+							//cout<<i<<","<<tj<<" <> "<<i-z<<","<<y<<"="<<tmp<<"\n";
+							if(tmp <= value){
+								if(dy > y){
+									value = tmp;
+									dx = i-z, dy = y;
+								}
+
+							}
+						}
+					}
+				}
+				if(value != 16*16){
+					kill.push_back(make_pair(dx, dy));
+				}
+			}
+			int size = kill.size();
+			for(int j=0;j<size;j++){
+				int x = kill[j].first, y = kill[j].second;
+				if(tmp_map[x][y] != 0) sum++;
+				tmp_map[x][y] = 0;
+			}
+			//print_map();
+		}
+		if(ans < sum){
+			//cout<<"sum:"<<sum<<"\n";
+			ans = sum;
+		}
+		return;
+	}
+
+	for(int i=cur;i<m;i++){
+		hunters.push_back(i);
+		solve(i + 1, cnt + 1);
+		hunters.pop_back();
+	}
+}
+int main()
+{
+	io;
+	cin>>n>>m>>d;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			cin>>map[i][j];
+		}
+	}
+	solve(0, 0);
+	cout<<ans<<"\n";
+	return 0;
+}
+
+/*
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -85,3 +185,4 @@ int main()
 	cout << ans << "\n";
 	return 0;
 }
+*/
