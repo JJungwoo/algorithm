d923544875f759f6c3d5656d603fb469ed8d96ac
JJungwoo <jungwoo5759@gmail.com>
2020-04-11 22:15:59 +0900

    - boj string 16916. 부분 문자열 solve update (kmp 알고리즘 적용, kmp 어렵다.. 암기하고 이해해보자)

--- baekjoon/string/16916.cpp
+++ baekjoon/string/16916.cpp
@@ -1,30 +1,51 @@
 /*
-[BOJ] 16916. Î∂ÄÎ∂Ñ Î¨∏ÏûêÏó¥ 
+[boj] 16916. ∫Œ∫– πÆ¿⁄ø≠
 */
+
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cstdio>
 using namespace std;
 #define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-string input, tmp;
+vector<int> getPi(string p) {
+	int m = (int)p.size(), j = 0;
+	vector<int> pi(m, 0);
+	for (int i = 1; i < m; i++) {
+		while (j > 0 && p[i] != p[j])
+			j = pi[j - 1];
+		if (p[i] == p[j])
+			pi[i] = ++j;
+	}
+	return pi;
+}
+vector<int> kmp(string s, string p) {
+	vector<int> ans;
+	auto pi = getPi(p);
+	int n = (int)s.size(), m = (int)p.size(), j = 0;
+	for (int i = 0; i < n; i++) {
+		while (j > 0 && s[i] != p[j])
+			j = pi[j - 1];
+		if (s[i] == p[j]) {
+			if (j == m - 1) {
+				ans.push_back(i - m + 1);
+				j = pi[j];
+			}
+			else {
+				j++;
+			}
+		}
+	}
+	return ans;
+}
+
 int main()
 {
-    io;
-    cin>>input>>tmp;
-    int ans = 0;
-    int isize = input.size(), tsize = tmp.size();
-    for(int i=0;i<isize - tsize;i++){
-        bool check = true;
-        for(int j=0;j<tsize;j++){
-            if(input[i + j] != tmp[j]) {check = false;
-                cout<<input[i]<<","<<tmp[j]<<"\n";
-            }
-        }
-        if(check) {
-            ans = 1;
-            break;
-        }
-    }
-    cout<<ans<<"\n";
+	io;
+	string s, p;
+	cin >> s >> p;
+	auto matched = kmp(s, p);
+	cout << !matched.empty() << "\n";
 
-    return 0;
+	return 0;
 }
