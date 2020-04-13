19e4a422f97c118344a8648d626855d32fe8a65d
JJungwoo <jungwoo5759@gmail.com>
2020-04-13 21:33:23 +0900

    -  boj simul 17244. 아맞다우산 code update (이상하네 10퍼 인가 그 이상으로 올라갈 때, 런타임 에러 발생;;)

--- baekjoon/simul/17244.cpp
+++ baekjoon/simul/17244.cpp
@@ -4,6 +4,102 @@ sort를 사용해서 물건의 위치중 가장 가까운것 먼저 찾는 방식을 사용하였는데, 문제�
 내위치와 물건사이에 벽을 생각못했다... 이런;
 물건 갯수는 최대 5개이니까 그냥 5개 찾아가는 순서만 돌리면 되는것이였음을..
 */
+
+
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cstring>
+using namespace std;
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int myX, myY, endX, endY;
+int n, m, xcnt, ans, minans = 987654321;
+char map[51][51];
+int dir[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
+struct checklist {
+	int x, y, mvcnt;
+};
+vector<checklist> v, sv;
+bool visitelist[5];
+void bfs(int tx, int ty)
+{
+	bool visited[51][51];
+	memset(visited, 0, sizeof(visited));
+	queue<checklist> q;
+	q.push({ myX,myY,0 });
+	visited[myX][myY] = true;
+	while (!q.empty()) {
+		int dx = q.front().x, dy = q.front().y;
+		int mvcnt = q.front().mvcnt;
+		q.pop();
+		if (tx == dx && ty == dy) {
+			myX = tx, myY = ty;
+			ans += mvcnt;
+			break;
+		}
+		for (int i = 0; i < 4; i++) {
+			int mx = dx + dir[i][0], my = dy + dir[i][1];
+			if (map[mx][my] == '#') continue;
+			if (visited[mx][my])continue;
+			visited[mx][my] = true;
+			q.push({ mx,my,mvcnt + 1 });
+		}
+	}
+}
+
+void solve(int cnt) {
+	if (cnt == xcnt) {
+		ans = 0;
+		int size = sv.size();
+		for (int i = 0; i < size; i++) {
+			//cout << sv[i].x << "," << sv[i].y << "\n";
+			bfs(sv[i].x, sv[i].y);
+		}
+		bfs(endX, endY);
+		if (minans > ans) {
+			minans = ans;
+		}
+		return;
+	}
+
+	for (int i = 0; i < xcnt; i++) {
+		if (visitelist[i]) continue;
+		visitelist[i] = true;
+		sv.push_back(v[i]);
+		solve(cnt + 1);
+		sv.pop_back();
+		visitelist[i] = false;
+	}
+}
+
+int main()
+{
+	io;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> map[i][j];
+			if (map[i][j] == 'S')
+				myX = i, myY = j;
+			else if (map[i][j] == 'X') {
+				v.push_back({ i,j,0 });
+				xcnt++;
+			}
+			else if (map[i][j] == 'E') {
+				endX = i, endY = j;
+			}
+		}
+	}
+	if (xcnt != 0) solve(0);
+	else {
+		bfs(endX, endY);
+		minans = ans;
+	}
+	cout << minans << "\n";
+	return 0;
+}
+
+
 /*
 #include <iostream>
 #include <queue>
@@ -92,93 +188,3 @@ int main()
 	return 0;
 }
 */
-
-#include <iostream>
-#include <queue>
-#include <vector>
-#include <cstdio>
-using namespace std;
-#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-int myX, myY, endX, endY;
-int n, m, xcnt, ans, minans = 987654321;
-char map[51][51];
-int dir[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
-struct checklist {
-	int x, y, mvcnt;
-};
-vector<checklist> v, sv;
-void bfs(int tx, int ty) 
-{
-	bool visited[51][51];
-	memset(visited, 0, sizeof(visited));
-	queue<checklist> q;
-	q.push({ myX,myY,0 });
-	visited[myX][myY] = true;
-	while (!q.empty()) {
-		int dx = q.front().x, dy = q.front().y;
-		int mvcnt = q.front().mvcnt;
-		q.pop();
-		if (tx == dx && ty == dy){
-			myX = tx, myY = ty;
-			ans += mvcnt;
-			break;
-		}
-		for (int i = 0; i < 4; i++) {
-			int mx = dx + dir[i][0], my = dy + dir[i][1];
-			if (map[mx][my] == '#') continue;
-			if (visited[mx][my])continue;
-			visited[mx][my] = true;
-			q.push({ mx,my,mvcnt + 1 });
-		}
-	}
-}
-
-void solve(int cur, int cnt) {
-	if (cnt == xcnt) {
-		ans = 0;
-		int size = sv.size();
-		for (int i = 0; i < size; i++) {
-			bfs(sv[i].x, sv[i].y);
-		}
-		bfs(endX, endY);
-		if (minans > ans) {
-			minans = ans;
-		}
-		return;
-	}
-
-	int size = v.size();
-	for (int i = 0; i < size; i++) {
-		sv.push_back(v[i]);
-		solve(i + 1, cnt + 1);
-		sv.pop_back();
-	}
-}
-
-int main()
-{
-	io;
-
-	cin >> n >> m;
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> map[i][j];
-			if (map[i][j] == 'S')
-				myX = i, myY = j;
-			else if (map[i][j] == 'X') {
-				v.push_back({ i,j,0 });
-				xcnt++;
-			}
-			else if (map[i][j] == 'E') {
-				endX = i, endY = j;
-			}
-		}
-	}
-	if(xcnt != 0) solve(0, 0);
-	else {
-		bfs(endX, endY);
-		minans = ans;
-	}
-	cout << minans << "\n";
-	return 0;
-}
