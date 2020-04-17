f3a5eb61f227685907af9f4006852b90dd301a3b
JJungwoo <jungwoo5759@gmail.com>
2020-04-17 22:32:44 +0900

    - boj hashing 1764 듣보잡 , 7662 이중 우선순위 큐  solve update (map, set 응용 문제 해싱 stl 사용법만 알고 있다면 손쉽게 해결 가능하다.)

---
+++ baekjoon/hashing/1764.cpp
@@ -0,0 +1,82 @@
+﻿/*
+[boj] 1764. 듣보잡
+*/
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+map<string, int> mp;
+int n, m;
+string tmp;
+vector<string> ans;
+
+int main()
+{
+	io;
+	cin >> n >> m;
+	while (n--) {
+		cin >> tmp;
+		mp[tmp]++;
+	}
+
+	while (m--) {
+		cin >> tmp;
+		mp[tmp]++;
+	}
+
+	for (auto it : mp) {
+		if (it.second >= 2) {
+			ans.push_back(it.first);
+		}
+	}
+	sort(ans.begin(), ans.end());
+	cout << ans.size() << "\n";
+	for (auto it : ans) {
+		cout << it << "\n";
+	}
+	return 0;
+}
+
+/*
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int n, m;
+multiset<string> nohear, nosee;
+vector<string> ans;
+int main()
+{
+	io;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		string tmp;
+		cin >> tmp;
+		nohear.insert(tmp);
+	}
+	for (int i = 0; i < m; i++) {
+		string tmp;
+		cin >> tmp;
+		auto it = nohear.find(tmp);
+		if (it != nohear.end()) {
+			ans.push_back(*it);
+		}
+	}
+
+	sort(ans.begin(), ans.end());
+	cout << ans.size() << "\n";
+	for (int i = 0; i < ans.size(); i++) {
+		cout << ans[i] << "\n";
+	}
+
+	return 0;
+}
+*/
---
+++ baekjoon/hashing/7662.cpp
@@ -0,0 +1,40 @@
+﻿/*
+[boj] 7662. 이중 우선순위 큐
+동일한 정수가 입력될 수 있다;;
+*/
+
+#include <iostream>
+#include <set>
+using namespace std;
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int t, n;
+int main()
+{
+	io;
+	cin >> t;
+	while (t--) {
+		multiset<int> s;
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			char cmd; cin >> cmd;
+			int value; cin >> value;
+			if(cmd == 'I') s.insert(value);
+			else if (cmd == 'D' && !s.empty()) {
+				if (value == 1) {
+					s.erase(--s.end());
+				}
+				else {
+					s.erase(s.begin());
+				}
+			}			
+		}
+
+		if (s.size() == 0) cout << "EMPTY\n";
+		else {
+			auto a = s.begin(), b = --s.end();
+			cout << *b << " " << *a << "\n";
+		}
+	}
+
+	return 0;
+}
