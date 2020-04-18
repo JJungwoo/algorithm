ddcd8555915f78e02e994115f05c5430fae58f36
JJungwoo <jungwoo5759@gmail.com>
2020-04-18 19:20:34 +0900

    - boj brute force 1182 부분수열의 합, 14225 부분수열의 합 solve update (둘다 완전 탐색 문제로 재귀 방법을 사용함. 나중에 다시 한번 풀어보길, 한번에 푸는 방법을 생각지 못하였다.

---
+++ baekjoon/brute_force/1182.cpp
@@ -0,0 +1,29 @@
+﻿/*
+[boj] 1182. 부분수열의 합
+*/
+#include <iostream>
+using namespace std;
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int n, s, ans;
+int numbers[20];
+void solve(int num, int sum){
+	if (num > n) return;
+	if (sum == s && num == n) {
+		ans++;
+		return;
+	}
+	solve(num + 1, sum + numbers[num]);
+	solve(num + 1, sum);
+}
+int main()
+{
+	io;
+	cin >> n >> s;
+	for (int i = 0; i < n; i++) {
+		cin >> numbers[i];
+	}
+	solve(0, 0);
+	if (s == 0) ans--;
+	cout << ans << "\n";
+	return 0;
+}
---
+++ baekjoon/brute_force/14225.cpp
@@ -0,0 +1,43 @@
+﻿/*
+[boj] 14225. 부분수열의 합
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int s, ans = 1;
+vector<int> seq;
+bool visited[2000001];
+void solve(int num, int sum) {
+	if (num > s) return;
+	if (num == s) {
+		visited[sum] = true;
+		if (ans < sum) {
+			ans = sum;
+		}
+		return;
+	}
+
+	solve(num + 1, sum + seq[num]);
+	solve(num + 1, sum);
+}
+int main()
+{
+	io;
+	cin >> s;
+	seq.resize(s);
+	for (int i = 0; i < s; i++) {
+		cin >> seq[i];
+	}
+	solve(0, 0);
+	bool find = false;
+	for (int i = 1; i <= ans; i++) {
+		if (visited[i]) continue;
+		find = true;
+		cout << i << "\n";
+		break;
+	}
+	if (!find) cout << ans + 1 << "\n";
+	return 0;
+}
