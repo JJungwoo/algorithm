cca01b171dd566b9d5a5ec5498486885882f813c
JJungwoo <jungwoo5759@gmail.com>
2020-04-24 16:07:19 +0900

    - boj simul 16926 배열 돌리기 solve update (배열 인덱스 문제로 생각보다 어려움;) - boj simul  9466 텀 프로젝트 code update (그래프 사이클 찾는 문제, 시간 초과 발생 원인 찾아야 함..)

---
+++ baekjoon/simul/16926.cpp
@@ -0,0 +1,57 @@
+﻿/*
+[boj] 16926. 배열 돌리기1
+*/
+#include <iostream>
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int n, m, r;
+int state = 0, nstate = 1;
+int map[301][301][2];
+int dir[4][2] = { {1,0},{0,1},{-1,0},{0,-1} };
+void print(int tmp[301][301][2])
+{
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			cout << tmp[i][j][state] << " ";
+		}
+		cout << "\n";
+	}
+}
+void solve()
+{
+	while (r--) {
+		int i = 1, j = 1, tn = n, tm = m;
+		while (i <= n / 2 && j <= m / 2) {
+			int tmp = r;
+			int ti = i, tj = j, d = 0;
+			do {
+				int nx = ti + dir[d][0], ny = tj + dir[d][1];
+				if (nx < i || ny < j || nx > tn || ny > tm) d++;
+				else {
+					map[nx][ny][nstate] = map[ti][tj][state];
+					ti = nx, tj = ny;
+				}
+			} while (!(ti == i && tj == j));
+			i++, j++;
+			tn--, tm--;
+		}
+		state = (state + 1) % 2;
+		nstate = (state + 1) % 2;
+	}
+}
+int main()
+{
+	io;
+	cin >> n >> m >> r;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			cin >> map[i][j][0];
+		}
+	}
+	//int mod = (n * 2) + ((m - 2) * 2);
+	//r = (r % mod);
+	//cout << r << "," << mod << "\n";
+	solve();
+	print(map);
+	return 0;
+}
---
+++ baekjoon/simul/9466.cpp
@@ -0,0 +1,46 @@
+﻿/*
+[boj] 9466. 텀 프로젝트
+시간초과.. 
+*/
+#include <iostream>
+#include <vector>
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int t, n;
+vector<int> v[100001];
+void init(int n) {
+	for (int i = 1; i <= n; i++) {
+		v[i].clear();
+	}
+}
+bool cycle(int idx, int des, int cnt) {
+	if (idx == des && cnt > 0) {
+		return true;
+	}
+	else if (cnt > n) return false;
+	int size = v[idx].size();
+	for (int i = 0; i < size; i++) {
+		if (cycle(v[idx][i], des, cnt + 1)) return true;
+	}
+	return false;
+}
+int main()
+{
+	io;
+	cin >> t;
+	while (t--) {
+		int ans = 0, value;
+		cin >> n;
+		init(n);
+		for (int i = 1; i <= n; i++) {
+			cin >> value;
+			v[i].push_back(value);
+		}
+		for (int i = 1; i <= n; i++) {
+			if (cycle(i, i, 0) == false) ans++;
+		}		
+		cout << ans << "\n";
+	}
+	
+	return 0;
+}
