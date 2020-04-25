12762d9984ec77b87cc0957765fc8297d4824d73
JJungwoo <jungwoo5759@gmail.com>
2020-04-25 21:34:56 +0900

    - boj graph 9466 텀 프로젝트 solve update (그래프 사이클 찾는 문제로 단순 dfs 돌리면 시간초과 발생하기 때문에 방문점과 사이클 처리를 따로 해줘야 한다. 다음에 또 풀어봐야할 문제) - boj bfs 1726 로봇 solve update (응용된 bfs 유형 문제로 다시 풀어봐야 한다. 실수가 자주 발생해서 생각보다 오래 걸림)

---
+++ baekjoon/bfs/1726.cpp
@@ -0,0 +1,63 @@
+﻿/*
+[boj] 1726. 로봇
+다시 한번 풀어보기.. 좋은 bfs문제인데 실수를 너무 많이했다.;
+*/
+#include <iostream>
+#include <queue>
+#include <cmath>
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int m, n, ans = 987654321;
+int map[100][100];
+int dir[4][2] = { {0,1},{0,-1},{1,0},{-1,0} };	// 동, 서, 남, 북
+bool visited[100][100][4];
+struct robot {
+	int x, y, d, cnt;
+};
+robot sspot, espot;
+int solve() {
+	queue<robot> q;
+	q.push({ sspot.x, sspot.y, sspot.d, 0 });
+	visited[sspot.x][sspot.y][sspot.d] = 1;
+	while (!q.empty()) {
+		int x = q.front().x, y = q.front().y;
+		int d = q.front().d, cnt = q.front().cnt;
+		q.pop();
+		if (x == espot.x && y == espot.y && d == espot.d) {
+			return cnt;
+		}
+		//for (int t = 3; t >= 1; t--) {
+		for (int t = 1; t <= 3; t++) {
+			int mx = x + (dir[d][0] * t), my = y + (dir[d][1] * t);
+			if (mx < 0 || my < 0 || mx >= m || my >= n) continue;
+			if (visited[mx][my][d]) continue;
+			if (map[mx][my] == 1) break;
+			visited[mx][my][d] = true;
+			q.push({ mx,my,d,cnt + 1 });
+		}
+		for (int i = 0; i < 4; i++) {
+			if (d == i) continue;
+			int k = ((d + i) % 4) == 1 ? 2 : 1;
+			if (visited[x][y][i]) continue;
+			visited[x][y][i] = true;
+			q.push({ x,y,i,cnt+k });
+		}
+	}
+}
+int main()
+{
+	io;
+	cin >> m >> n;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> map[i][j];
+		}
+	}
+	cin >> sspot.x >> sspot.y >> sspot.d;
+	sspot.x--, sspot.y--, sspot.d--;
+	cin >> espot.x >> espot.y >> espot.d;
+	espot.x--, espot.y--, espot.d--;
+	ans = solve();
+	cout << ans << "\n";
+	return 0;
+}
---
+++ baekjoon/graph/9466.cpp
@@ -0,0 +1,51 @@
+﻿/*
+[boj] 9466. 텀 프로젝트
+시간초과.. 
+방문 체크를 해줘야함.. 어렵다;
+*/
+#include <iostream>
+#include <vector>
+#include <cstring>
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int t, n, ans;
+int student[100001];
+bool visited[100001], done[100001];
+void cycle(int idx) {
+	visited[idx] = true;
+	int next = student[idx];
+	//cout << idx << ":" << next << "\n";
+	if (!visited[next]) {
+		cycle(next);
+	}
+	else {
+		if (!done[next]) {
+			for (int i = next; i != idx; i = student[i]) {
+				//cout << i << "\n";
+				ans++;
+			}
+			ans++;
+		}
+	}
+	done[idx] = true;
+}
+int main()
+{
+	io;
+	cin >> t;
+	while (t--) {
+		cin >> n;
+		for (int i = 1; i <= n; i++) {
+			cin >> student[i];
+		}
+		memset(visited, 0, sizeof(visited));
+		memset(done, 0, sizeof(done));
+		ans = 0;
+		for (int i = 1; i <= n; i++) {
+			if (visited[i]) continue;
+			cycle(i);
+		}		
+		cout << n - ans << "\n";
+	}
+	return 0;
+}
