2a12fba37c3d7c77450b11a7e358cbcc90af7c13
JJungwoo <jungwoo5759@gmail.com>
2020-04-27 20:55:02 +0900

    - boj simul 14499 주사위 굴리기, 2933 미네랄, 3190 뱀  solve update (3문제 모두 좋은 시뮬 문제, 다시 풀면서 구현 방법으로 익혀두면 좋을 듯) (주사위 굴리기는 생각이 좀 필요한 시뮬, 미네랄은 생각해보면 단순한 시뮬인데 어떻게 접근하고 구현할지 쉽지는 않은듯, 뱀은 큐를 응용한 느낌으로 좋은 시뮬 문제 꼭 다시 풀자)

---
+++ baekjoon/simul/14499.cpp
@@ -0,0 +1,82 @@
+﻿/*
+[boj] 14499. 주사위 굴리기
+(r,c) = (x,y);
+지도의 각 칸에는 정수가 하나씩 쓰여져 있다. 주사위를 굴렸을 때, 
+이동한 칸에 쓰여 있는 수가 0이면, 주사위의 바닥면에 쓰여 있는 수가 칸에 복사된다. 
+0이 아닌 경우에는 칸에 쓰여 있는 수가 주사위의 바닥면으로 복사되며, 
+칸에 쓰여 있는 수는 0이 된다.
+주사위를 놓은 곳의 좌표와 이동시키는 명령이 주어졌을 때, 
+주사위가 이동했을 때 마다 상단에 쓰여 있는 값을 구하는 프로그램을 작성하시오.
+만약 바깥으로 이동시키려고 하는 경우에는 해당 명령을 무시해야 하며, 출력도 하면 안 된다.
+*/
+#include <iostream>
+#include <vector>
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int n, m, x, y, k;
+int map[20][20], dirs[1001];
+int dir[4][2] = { {0,1},{0,-1},{-1,0},{1,0} };	// 동 서 북 남
+int dice[6][2];	// 주사위 윗면 동 서 북 남 밑변
+void solve()
+{
+	int cur = 0, next = 1;
+	for (int i = 0; i < k; i++) {
+		int d = dirs[i] - 1;
+		int mx = x + dir[d][0], my = y + dir[d][1];
+		if (mx < 0 || my < 0 || mx >= n || my >= m) continue;
+		if (map[mx][my] == 0) {
+			map[mx][my] = dice[dirs[i]][cur];
+		}
+		else {		// dice[dirs[i]] 가는 방향에서 밑면으로 바뀜
+			dice[dirs[i]][cur] = map[mx][my];
+			map[mx][my] = 0;
+		}
+		dice[5][next] = dice[dirs[i]][cur];	// 밑면
+
+		// 동 <-> 서 , 0 <-> 1
+		// 북 <-> 남 , 2 <-> 3
+		dice[dirs[i]][next] = dice[0][cur];	// 현재 윗면이 가는 방향으로 
+
+		// 윗면은 가는 방향의 반대면의 값
+		if (d == 0 || d == 1) {
+			int vd = ((d == 0) ? 2 : 1);
+			// 0이면 동 <-> 서(dice에서는 2) 반대도 똑같이
+			dice[0][next] = dice[vd][cur];
+			// 가는 방향의 반대면은 현재 아래면을 가는 방향의 반대면에 추가
+			dice[vd][next] = dice[5][cur];
+			// 양옆면은 변화없음
+			dice[3][next] = dice[3][cur];
+			dice[4][next] = dice[4][cur];
+		}
+		else if (d == 2 || d == 3) {
+			int vd = ((d == 2) ? 4 : 3);
+			dice[0][next] = dice[vd][cur];
+			dice[vd][next] = dice[5][cur];
+			dice[1][next] = dice[1][cur];
+			dice[2][next] = dice[2][cur];
+		}
+
+		// 윗면 출력
+		cout << dice[0][next] << "\n";
+
+		// 상태 변환
+		next = cur;
+		cur = (cur + 1) % 2;
+		x = mx, y = my;
+	}
+}
+int main()
+{
+	io;
+	cin >> n >> m >> x >> y >> k;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> map[i][j];
+		}
+	}
+	for (int i = 0; i < k; i++) {
+		cin >> dirs[i];
+	}
+	solve();
+	return 0;
+}
---
+++ baekjoon/simul/2933.cpp
@@ -0,0 +1,147 @@
+﻿/*
+[boj] 2933. 미네랄
+*/
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+int r, c, n, turn, clusterCnt;
+char map[100][100];
+int del[100];
+int dir[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
+bool passCluster[101];
+int visited[100][100];
+struct pos {
+	int x, y;
+};
+
+bool compare(pos a, pos b) {
+	return a.x > b.x;
+}
+void bfs(int x, int y, vector<pos> cluster[101]) {
+	queue<pos> q;
+	q.push({ x,y });
+	visited[x][y] = clusterCnt;
+	cluster[clusterCnt].push_back(q.front());
+	while (!q.empty()) {
+		int dx = q.front().x, dy = q.front().y;
+		q.pop();
+		cluster[clusterCnt].push_back({ dx, dy });
+		if (dx == r - 1) {
+			passCluster[clusterCnt] = true;
+		}
+		for (int i = 0; i < 4; i++) {
+			int mx = dx + dir[i][0], my = dy + dir[i][1];
+			if (mx < 0 || my < 0 || mx >= r || my >= c) continue;
+			if (map[mx][my] != 'x') continue;
+			if (visited[mx][my] != -1)continue;
+			visited[mx][my] = clusterCnt;
+			//cluster[clusterCnt].push_back({ mx, my });
+			q.push({ mx,my });
+		}
+	}
+}
+void print() {
+	cout << "print\n";
+	for (int x = 0; x < r; x++) {
+		for (int y = 0; y < c; y++) {
+			cout << visited[x][y] <<"   ";
+		}
+		cout << "\n";
+	}
+}
+void solve()
+{
+	for (int i = 0; i < n; i++) {
+		vector<pos> cluster[101];
+		int idx = (r - 1) - del[i];
+		if (!turn){	// 왼
+			for (int j = 0; j < c; j++) {
+				if (map[idx][j] == 'x') {
+					map[idx][j] = '.';
+					break;
+				}
+			}
+		}
+		else {
+			for (int j = c - 1; j >= 0; j--) {
+				if (map[idx][j] == 'x') {
+					map[idx][j] = '.';
+					break;
+				}
+			}
+		}
+		
+		// cluster check bfs
+		memset(visited, -1, sizeof(visited));
+		memset(passCluster, 0, sizeof(passCluster));
+		for (int x = 0; x < r; x++) {
+			for (int y = 0; y < c; y++) {
+				if (map[x][y] == 'x' && visited[x][y] == -1) {
+					bfs(x, y, cluster);
+					clusterCnt++;
+				}
+			}
+		}
+		//print();
+		for (int c = 0; c < clusterCnt; c++) {
+			//cout << c << " is pass! : " << passCluster[c] << "\n";
+			if (passCluster[c]) {
+				continue;
+			}
+			sort(cluster[c].begin(), cluster[c].end(), compare);
+			int csize = cluster[c].size();
+			//cout << csize << "\n";
+			bool check = false;
+			while (csize > 0) {
+				//cout << "cluster : \n";
+				for (int loop = 0; loop < csize; loop++) {
+					int mx = cluster[c][loop].x + dir[1][0], my = cluster[c][loop].y + dir[1][1];
+					//cout << mx << "," << my << "\n";
+					if (mx >= r || (map[mx][my] == 'x' && visited[mx][my] != c)) {
+						//cout << mx << "," << my << "\n";
+						check = true;
+						break;
+					}
+				}
+				if (check) break;
+				else {		// 내려주기
+					for (int loop = 0; loop < csize; loop++) {
+						int mx = cluster[c][loop].x + dir[1][0], my = cluster[c][loop].y + dir[1][1];
+						map[mx][my] = 'x';
+						map[cluster[c][loop].x][cluster[c][loop].y] = '.';
+						visited[mx][my] = c;
+						visited[cluster[c][loop].x][cluster[c][loop].y] = -1;
+						cluster[c][loop].x = mx, cluster[c][loop].y = my;
+					}
+				}
+			}
+		}
+		turn = (turn + 1) % 2;
+		clusterCnt = 0;
+	}
+}
+int main()
+{
+	cin >> r >> c;
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cin >> map[i][j];
+		}
+	}
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> del[i];
+		del[i]--;
+	}
+	solve();
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cout << map[i][j];
+		}
+		cout << "\n";
+	}
+	return 0;
+}
