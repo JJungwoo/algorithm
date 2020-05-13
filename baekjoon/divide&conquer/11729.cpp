f26dcd53210fd72b9dd2ff0fe4263231bc589e60
JJungwoo <jungwoo5759@gmail.com>
2020-05-13 23:03:19 +0900

    새롭게 분할 정복과 그리디 문제를 풀다보니 풀이 도출과정이 아직 익숙하지 않다. 반복해서 더 풀어봐야할 듯하다. - boj divide and conquer 11729. 하노이 탑 이동 순서 solve update (분할 정복 문제로 주어진 문제에서 귀납법을 찾아 재귀로 구현, 귀납법 도출 어려움;;) - boj greedy 2875. 대회 or 인턴 solve update (그리디 기본 문제, 간단하게 기저 조건만 맞춰서 돌리면 되는데... 헤맴;;)

---
+++ baekjoon/divide&conquer/11729.cpp
@@ -0,0 +1,29 @@
+﻿/*
+[boj] 11729. 하노이 탑 이동 순서
+한 번에 한 개의 원판만을 다른 탑으로 옮길 수 있다.
+쌓아 놓은 원판은 항상 위의 것이 아래의 것보다 작아야 한다.
+하노이 탑 귀납법: 2n - 1
+*/
+
+#include <iostream>
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int n, k;
+void hanoi(int n, int a, int b) {
+	if (n == 0) return;
+
+	hanoi(n - 1, a, 6 - a - b);
+	cout << a << " " << b << "\n";
+	hanoi(n - 1, 6 - a - b, b);
+}
+int main()
+{
+	io;
+	cin >> n;
+	
+	cout << (1<<n) - 1 << "\n";
+	
+	hanoi(n, 1, 3);
+	
+	return 0;
+}
---
+++ baekjoon/greedy/2875.cpp
@@ -0,0 +1,25 @@
+﻿/*
+[boj] 2875. 대회 or 인턴
+*/
+
+#include <iostream>
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int n, m, k;
+int main()
+{
+	io;
+	cin >> n >> m >> k;
+	int ans = 0;
+	int total = n + m - k;
+
+	while (n - 2 >= 0 && m - 1 >= 0 && total - 3 >= 0) {
+		n -= 2;
+		m--;
+		total -= 3;
+		ans++;
+	}
+	cout << ans << "\n";
+	return 0;
+}
+
