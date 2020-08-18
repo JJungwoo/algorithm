49faaf785070b83581ca75efe4fef8e30920c13f
jungwoo <jungwoo5759@gmail.com>
2020-08-18 19:20:22 +0900

    - daily coding 0818 boj solve update

---
+++ baekjoon/daily_coding/0818/17436.cpp
@@ -0,0 +1,16 @@
+/*
+[boj] 17436. 소수의 배수
+https://www.acmicpc.net/problem/17436
+*/
+
+#include <iostream>
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+int main() 
+{
+    io;
+
+    return 0;
+}
+
