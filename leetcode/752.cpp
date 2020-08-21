62ae2f3838fc065ed68c5556018d1c1f3e47f965
jungwoo <jungwoo5759@gmail.com>
2020-08-21 23:59:57 +0900

    - leetcode 752. Open the Lock solve update

---
+++ leetcode/752.cpp
@@ -0,0 +1,77 @@
+/*
+[LeetCode] 752. Open the Lock
+https://leetcode.com/problems/open-the-lock/
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <set>
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+set<string> trap, path;
+
+class Solution {
+public:
+    unordered_set<string> trap, path;
+    const int INF = 987654321;
+    int ans = INF;
+    int move[2] = {1, -1};
+    string endpoint;
+    void bfs() {
+        queue<pair<string, int> > q;
+        q.push(make_pair("0000", 0));
+        while(!q.empty()){
+            string tmp = q.front().first;
+            int cnt = q.front().second;
+            q.pop();
+            if(trap.find(tmp) != trap.end()) continue;
+            if(endpoint.compare(tmp) == 0){
+                ans = cnt;
+                break;
+            }
+
+            for(int i=0;i<2;i++){
+                for(int j=0;j<4;j++){
+                    string buf = tmp;
+                    if(move[i] > 0) buf[j] = buf[j] + 1 > '9' ? '0' : buf[j] + 1;
+                    else buf[j] = buf[j] - 1 < '0' ? '9' : buf[j] - 1;
+                    if(path.find(buf) != path.end()) continue;
+                    path.insert(buf);
+                    q.push(make_pair(buf, cnt+1));
+                }
+            }
+        }
+    }
+
+    int openLock(vector<string>& deadends, string target) {
+        endpoint = target;
+        for(int i=0;i<deadends.size();i++)
+            trap.insert(deadends[i]);
+        bfs();
+        if(ans == INF) 
+            return -1;
+        return ans;
+    }
+};
+
+vector<string> ex_deadends;
+string ex_target = "0202";
+
+int main()
+{
+    io;
+
+    ex_deadends.push_back("0201");
+    ex_deadends.push_back("0101");
+    ex_deadends.push_back("0102");
+    ex_deadends.push_back("1212");
+    ex_deadends.push_back("2002");
+
+    Solution solution = Solution();
+    cout<<"ans: "<<solution.openLock(ex_deadends, ex_target)<<"\n";
+
+    return 0;
+}
