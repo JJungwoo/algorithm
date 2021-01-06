30ff5fce5bae80fdc2e6507c8159363825617ce8
jungwoo <jungwoo5759@gmail.com>
2021-01-06 23:35:00 +0900

    CLEANUP: boj 2056. 작업 solve update

--- baekjoon/graph/2056.cpp
+++ baekjoon/graph/2056.cpp
@@ -1,7 +1,52 @@
 /*
 [boj] 2056. 작업
+https://www.acmicpc.net/problem/2056
 */
 
+#include<iostream>
+#include<vector>
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int N, complete_time[10001];
+int worktime[10001], refcnt[10001];
+vector<int> work[10001];
+void working(int idx) {
+    for(auto nxt : work[idx]) {
+        refcnt[nxt]--;
+        complete_time[nxt] = max(complete_time[nxt], complete_time[idx] + worktime[nxt]);
+        if(refcnt[nxt] == 0) {
+            refcnt[nxt] = -1;
+            working(nxt);
+        }
+    }
+}
+int main() {
+    io;
+    cin >> N;
+    for(int i=1;i<=N;i++) {
+        int wt; cin >> wt;
+        worktime[i] = wt;
+        int num; cin >> num;
+        while(num--) {
+            int first; cin >> first;
+            work[first].push_back(i);
+            refcnt[i]++;
+        }
+    }
+    for(int i=1;i<=N;i++)
+        if(refcnt[i] == 0) {
+            complete_time[i] = worktime[i];
+            working(i);
+        }
+    int ans = 0; 
+    for(int i=1;i<=N;i++) {
+        ans = max(ans, complete_time[i]);
+    }
+    cout<<ans<<"\n";
+    return 0;
+}
+
+/*
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -54,3 +99,5 @@ int main()
     
     return 0;
 }
+
+*/
