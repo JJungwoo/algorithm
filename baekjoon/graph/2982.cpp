4e52c462b6a69cd9931dedac475bdf150f6a90c0
JJungwoo <jungwoo5759@gmail.com>
2021-01-14 00:21:06 +0900

    ADD: add boj 2696. 중앙값 구하기 solve update

---
+++ baekjoon/graph/2982.cpp
@@ -0,0 +1,25 @@
+/*
+[boj] 2982. 국왕의 방문
+https://www.acmicpc.net/problem/2982
+*/
+
+#include <iostream>
+#include <vector>
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int N, M;
+int main()
+{
+    io;
+    cin >> N >> M;
+    int A, B, K, G;
+    cin >> A >> B >> K >> G;
+    while(G--) {
+
+    }
+    while(M--) {
+        int U, V, L;
+        cin >> U >> V >> L;
+    }
+    return 0;
+}
