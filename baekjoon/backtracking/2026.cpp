159e2934ec447b51d76c6c53f7f4aaca8b1531f9
JJungwoo <jungwoo5759@gmail.com>
2021-02-25 01:48:54 +0900

    add: add boj 2026. 소풍 solve upedate

--- baekjoon/backtracking/2026.cpp
+++ baekjoon/backtracking/2026.cpp
@@ -6,48 +6,8 @@ https://www.acmicpc.net/problem/2026
 #include <bits/stdc++.h>
 #define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
-int K, N, F;
-bool relation[901][901];
-int main()
-{
-	io;
-	cin >> K >> N >> F;
-	for(int i=0;i<F;i++) {
-		int A, B;
-		cin >> A >> B;
-		relation[A][B] = relation[B][A] = 1;
-	}
-
-	vector<int> v;
-	for(int i=1;i<=N;i++)
-		v.push_back(i);
-	do{
-		bool ispossible = true;
-		for(int i=0;i<K-1;i++) {
-			//cout<<v[i]<<" ";
-			for(int j=i+1;j<K;j++) {
-				if(!relation[v[i]][v[j]]) {
-					//cout<<"\n";
-					ispossible = false;
-					break;
-				}
-			}
-		}
-		if(ispossible == true) {
-			sort(v.begin(), v.begin()+K);
-			for(int i=0;i<K;i++) {
-				cout<<v[i]<<"\n";
-			}
-			return 0;
-		}
-	}while(next_permutation(begin(v), end(v)));
-	cout<<"-1\n";
-	return 0;
-}
-/*
-vector<int> v[901], ans;
 bool relation[901][901], ispossible;
-map<int, int> m;
+vector<int> ans;
 int K, N, F;
 void find(int pos) {
 	ans.push_back(pos);
@@ -55,10 +15,17 @@ void find(int pos) {
 		ispossible = true;
 		return;
 	}
-	for(auto nxt : v[pos]) {
+	for(int i=pos+1;i<=N;i++) {
+		bool check = true;
+		if(!relation[pos][i]) continue;
 		for(auto it : ans)
-			if(!relation[nxt][it]) return;
-		find(nxt);
+			if(!relation[i][it]) {
+				check = false;
+				break;
+			}
+		if(check)
+			find(i);
+		if(ispossible) return;
 	}
 	ans.pop_back();
 }
@@ -68,8 +35,6 @@ int main()
 	cin >> K >> N >> F;
 	for(int i=0;i<F;i++) {
 		int A, B; cin >> A >> B;
-		v[A].push_back(B);
-		v[B].push_back(A);
 		relation[A][B] = relation[B][A] = 1;
 	}
 
@@ -86,4 +51,3 @@ int main()
 	cout<<"-1\n";
     return 0;
 }
-*/
