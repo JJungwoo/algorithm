b32aede258103908e27317d8fb4131bfd40bf696
JJungwoo <jungwoo5759@gmail.com>
2021-02-26 01:28:04 +0900

    add: add boj 11718. 그대로 출력하기 solve update

---
+++ baekjoon/string/11718.cpp
@@ -0,0 +1,16 @@
+/*
+[boj] 11718. 그대로 출력하기
+https://www.acmicpc.net/problem/11718
+*/
+#include <bits/stdc++.h>
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main()
+{
+    io;
+	string input;
+	while(getline(cin, input))
+		cout<<input<<"\n";
+
+    return 0;
+}
