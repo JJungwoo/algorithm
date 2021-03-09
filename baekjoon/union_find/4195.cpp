e2575c3dd5bd318408a1b1785d9c87e4e46dbd24
JJungwoo <jungwoo5759@gmail.com>
2021-03-09 23:16:59 +0900

    add boj 4195. 친구 네트워크 solve update

--- baekjoon/union_find/4195.cpp
+++ baekjoon/union_find/4195.cpp
@@ -1,93 +1,52 @@
 /*
-[boj] 4195. Ä£±¸ ³×Æ®¿öÅ©
-°è¼Ó ·±Å¸ÀÓ ¿¡·¯ ¶ä;; ¿Ö±×·±°ÅÁö.. ÇÏ¾Æ..
-Áß°£¿¡ ·±Å¸ÀÓ¿¡·¯..
+[boj] 4195. ì¹œêµ¬ ë„¤íŠ¸ì›Œí¬
+https://www.acmicpc.net/problem/4195
 */
 
-#include <iostream>
-#include <map>
-#include <string>
-#include <algorithm>
-#include <cstdio>
+#include <bits/stdc++.h>
 #define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
-int parent[200011], usize[200011];
+int parent[200010], cnt[200010];
+int T, F;
+unordered_map<string, int> m;
 void init(int n) {
-	for (int i = 0; i < n; i++) {
-		parent[i] = i;
-		usize[i] = 1;
-	}
+    for(int i=0;i<2 * n;i++) {
+        parent[i] = i;
+        cnt[i] = 1;
+    }
 }
-
 int find(int p) {
-	if (parent[p] == p) return p;
-	else parent[p] = find(parent[p]);
+    if(parent[p] == p) return p;
+    else return parent[p] = find(parent[p]);
 }
-
-void uni(int p, int q) {
-	p = find(p);
-	q = find(q);
-	if (q == p) return;
-	if (usize[q] < usize[p]) {
-		parent[q] = p;
-		usize[p] += usize[q];
-	}
-	else {
-		parent[p] = q;
-		usize[q] += usize[p];
-	}
+int uni(int p, int q) {
+    p = find(p);
+    q = find(q);
+    if(p != q) {
+        parent[q] = p;
+        cnt[p] += cnt[q];
+        cnt[q] = 1;
+    }
+    return cnt[p];
 }
-
-int main()
+int main() 
 {
-	io;
-	int t; 
-	cin >> t;
-	while (t--) {
-		map<string, int> m;
-		int cnt = 0;
-		int f; cin >> f;
-		init(f * 2);
-		for (int i = 0; i < f; i++) {
-			string a, b;
-			cin >> a >> b;
-			if (m.count(a) == 0) {
-				m.insert(make_pair(a, cnt++));
-			}
-			if (m.count(b) == 0) {
-				m.insert(make_pair(b, cnt++));
-			}
-			uni(m[a], m[b]);
-			int tmp = find(m[a]);
-			//cout << tmp << ":" << usize[tmp] <<":"<<usize[m[a]]<<":"<<parent[m[a]]<< "\n";
-			//cout << usize[parent[m[a]]] << "\n";
-			cout << usize[tmp] << "\n";
-		}
-	}
-	return 0;
+    io;
+    cin >> T;
+    while(T--) {
+        int idx = 0;
+        m.clear();
+        cin >> F;
+        init(F);
+        for(int i=0;i<F;i++) {
+            string A, B;
+            cin >> A >> B;
+            if(m.find(A) == m.end())
+                m[A] = idx++;
+            if(m.find(B) == m.end())
+                m[B] = idx++;
+            cout<<uni(m[A], m[B])<<"\n";
+        }
+    }
+    return 0;
 }
-
-/*
-
-// c++ stl map »ç¿ë¹ı
-
-	#include <map>
-
-	map<string, int> m;
-
-	m.insert(make_pair("test0", 0));
-	m.insert(make_pair("test1", 1));
-	m.insert(make_pair("test2", 2));
-	m.insert(make_pair("test3", 3));
-
-	for (auto it = m.begin(); it != m.end(); it++) {
-		cout << it->first << "," << it->second << "\n";
-	}
-
-	cout << m.find("test0")->first << m.find("test0")->second << "\n";
-	//cout << m.find("test5")->first << m.find("test5")->second << "\n";
-	auto it = m.find("test5");
-	if (it == m.end()) cout << "fail\n";
-	else cout << it->first << "," << it->second << "\n";
-
-*/
