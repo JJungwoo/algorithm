74a52fa068212d75f949b4769e133f096499b865
JJungwoo <jungwoo5759@gmail.com>
2021-04-16 23:48:11 +0900

    add boj 1005. ACM Craft solve

--- baekjoon/graph/1005.cpp
+++ baekjoon/graph/1005.cpp
@@ -1,7 +1,80 @@
 /*
-[boj] 1005. ACM Crat
+[boj] 1005. ACM Craft
 https://www.acmicpc.net/problem/1005
 */
+
+#include <bits/stdc++.h>
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+#define START_POINT 0
+#define MAX_INT 1e9
+int N, K, W;
+int building[1001];
+vector<int> dp, way_count;
+vector<int> pos[1001];
+void all_clear() {
+    dp.clear();
+    dp.resize(N, -MAX_INT);
+    way_count.clear();
+    way_count.resize(N, 0);
+    for(int i=0;i<N;i++) {
+        pos[i].clear();
+    }
+}
+bool go_build(int idx) {
+    queue<int> q;
+    q.push(idx);
+
+    while(!q.empty()) {
+        int cur = q.front(); q.pop();
+        if (cur == W) {
+            return true;
+        }
+        for(int nxt : pos[cur]) {
+            if (--way_count[nxt] == 0) {
+                q.push(nxt);
+            }
+            if (dp[nxt] < dp[cur] + building[nxt]) {
+                dp[nxt] = dp[cur] + building[nxt];
+            }
+        }
+    }
+    return false;
+}
+int main() 
+{
+    io;
+    int T;
+    cin >> T;
+    while(T--) {
+        cin >> N >> K;
+        
+        all_clear();
+        for(int i=0;i<N;i++) {
+            cin >> building[i];
+        }
+        for(int i=0;i<K;i++) {
+            int X, Y;
+            cin >> X >> Y;
+            pos[X-1].push_back(Y-1);
+            way_count[Y-1]++;
+        }
+        cin >> W;
+        W--;
+        // find start point
+        for(int i=0;i<N;i++)
+            if (way_count[i] == START_POINT && dp[i] == -MAX_INT) {
+                dp[i] = building[i];
+                if(go_build(i)) break;
+            }
+        cout << dp[W] << "\n";
+    }
+    return 0;
+}
+
+/*
+
+// dfs 시간초과..
 
 #include <bits/stdc++.h>
 #define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
@@ -62,3 +135,4 @@ int main()
     }
     return 0;
 }
+*/
