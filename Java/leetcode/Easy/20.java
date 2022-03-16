faadecb499dad06cd4552e1cd8ea482206affeb5
jungwoo jo <jungwoo5759@gmail.com>
2022-03-16 23:10:32 +0900

    Update LeetCode 20. Valid Parentheses solve

---
+++ Java/leetcode/Easy/20.java
@@ -0,0 +1,21 @@
+/*
+[LeetCode] 20. Valid Parentheses
+https://leetcode.com/problems/valid-parentheses/
+*/
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
